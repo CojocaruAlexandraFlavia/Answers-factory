1ef65236306598957d8820d9c526b71eefa16bab
alexa <alexandraflavia00@gmal.com>
2022-11-03 20:54:40 +0200

    Created project

---
+++ src/main/java/com/example/answersfactory/AnswersFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.answersfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnswersFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnswersFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/answersfactory/AnswersFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.answersfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnswersFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
